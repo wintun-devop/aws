bd7ff67fcd702c1337610d459e9a5cbbd942a3d3
wintun-devop <wintun.hlaing@outlook.com>
2021-11-23 00:35:57 +0630

    v1.002

--- aws-terraform/ec2-single-user-data/apache_php8.0_ubuntu20.04lts.sh
+++ aws-terraform/ec2-single-user-data/apache_php8.0_ubuntu20.04lts.sh
@@ -31,6 +31,9 @@ apt update -y
 #install php8.0 modules
 apt install -y php8.0 libapache2-mod-php8.0
 
+#Installing additional php modules
+apt install php8.0-common php8.0-mysql php8.0-xml php8.0-curl php8.0-gd php8.0-imagick php8.0-cli php8.0-dev php8.0-imap php8.0-mbstring php8.0-opcache php8.0-soap php8.0-zip -y
+
 #check the php version
 php --version
 
